0220ebd2250108ff3df973585fac1ccd7427a0e0
serinko <johanesxsalmon@riseup.net>
2022-06-15 11:45:46 +0200

    todo

--- whallets-cli.py
+++ whallets-cli.py
@@ -1,1 +1,6 @@
-cd src
+"""CLI for users to manage the wallet database and run research functions"""
+# TODO:
+# 1) Interacting with APIs
+# 2) easy adding data to the wallets_dict.json
+# 3) Scanning through APIs based on given parameters
+# 4) Prints options for the user (-h)
