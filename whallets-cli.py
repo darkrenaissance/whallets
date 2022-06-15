1470efdc3c38576510e71e345b8eb9529741e287
serinko <johanesxsalmon@riseup.net>
2022-06-15 11:49:04 +0200

    get wallets

--- whallets-cli.py
+++ whallets-cli.py
@@ -1,6 +1,17 @@
 """CLI for users to manage the wallet database and run research functions"""
-# TODO:
+#TODO:
 # 1) Interacting with APIs
 # 2) easy adding data to the wallets_dict.json
 # 3) Scanning through APIs based on given parameters
 # 4) Prints options for the user (-h)
+
+import json
+
+def get_wallets():
+    ''' Gets the info from the dictionary '''
+    filename = 'wallets_dict.json'
+    with open(filename) as f:
+        all_wallets = json.load(f)
+    evm_wallets = all_wallets['evm_wallets']
+    spl_wallets = all_wallets['spl_wallets']
+    return evm_wallets, spl_wallets
